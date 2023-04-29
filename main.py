e42dbe489cf7cda3960d1c936d818d2234cd0e10
alantapravo <alantapravo@gmail.com>
2023-04-29 15:59:44 +0300

    add HomeWork 5 Виправлені помилки

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-N = int(input('Введіть число: '))
-
-if N %2 and N <= 20:
-    print("Weird")
-    exit()
-if not N %2 and 2 <= N <= 5:
+N = int(input("Введіть число:"))
+if not N % 2 and 2 <= N <= 5:
     print("Not Weird")
     exit()
-if not N %2 and 6 <= N <= 20:
+if not N % 2 and 6 <= N <= 20:
     print("Weird")
     exit()
-if N %2 and N >= 20:
+if N % 2 and N >= 20:
     print("Not Weird")
+    exit()
+if N % 2 and N <= 20:
+    print("Weird")
     exit()
